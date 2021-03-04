ca252ece6f3c1acaa00315d4ad7c0da40cf271f4
MahmoudCalipso <ammarmahmoud103@hotmail.fr>
2021-03-04 20:58:06 +0100

    Change Template & structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,40 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from '../app/Views/login/login.component';
-import { HomeComponent } from '../app/Views/home/home.component';
-import { RegisterComponent } from '../app/Views/register/register.component';
-import { ProfileComponent } from '../app/Views/profile/profile.component';
-import { TrackingComponent } from '../app/Views/tracking/tracking.component';
-import { AddFamilyCarComponent } from './Views/add-family-car/add-family-car.component';
+
 import { AdminGuard } from './Helper/admin.guard';
+import { AddUserComponent } from './Views/User/add-user/add-user.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'signin', component: LoginComponent },
-  { path: 'signup', component: RegisterComponent },
-  {
-    path: 'profile', component: ProfileComponent,
-    canActivate: [ AdminGuard ]
-  },
-  {
-    path: 'FamilyCar', component: AddFamilyCarComponent,
-    canActivate: [AdminGuard]
-  },
-  {
-    path: 'FamilyCar/:id', component: AddFamilyCarComponent,
-    canActivate: [AdminGuard]
-  },
-  {
-    path: 'tracking', component: TrackingComponent,
-    canActivate: [AdminGuard]
-  },
 
+  { path: 'test' , component: AddUserComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,52 +11,28 @@ import { FamilyCarModule } from './Models/family-car/family-car.module';
 import { LocationModule } from './Models/location/location.module';
 import { TypeCarModule } from './Models/type-car/type-car.module';
 import { UserModule } from './Models/user/user.module';
-import { LoginComponent } from './Views/login/login.component';
-import { RegisterComponent } from './Views/register/register.component';
-import { HomeComponent } from './Views/home/home.component';
-import { ProfileComponent } from './Views/profile/profile.component';
+
 import { authInterceptorProviders } from './helper/auth.interceptor';
 
-import { AgmCoreModule } from '@agm/core';
-import { TrackingComponent } from './Views/tracking/tracking.component';
+
+
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './Views/navbar/navbar.component';
+import { LeftbarComponent } from './Views/leftbar/leftbar.component';
 import { FooterComponent } from './Views/footer/footer.component';
-import { ScrollebarComponent } from './Views/scrollebar/scrollebar.component';
-import { ListeUsersComponent } from './Views/liste-users/liste-users.component';
-import { AddUsersComponent } from './Views/add-users/add-users.component';
-import { EditUsersComponent } from './Views/edit-users/edit-users.component';
-import { ListeTrackingComponent } from './Views/liste-tracking/liste-tracking.component';
-import { AddCarComponent } from './Views/add-car/add-car.component';
-import { ListeCarComponent } from './Views/liste-car/liste-car.component';
-import { EditCarComponent } from './Views/edit-car/edit-car.component';
-import { AddFamilyCarComponent } from './Views/add-family-car/add-family-car.component';
-import { ListFamilyCarComponent } from './Views/list-family-car/list-family-car.component';
-import { AddTypeCarComponent } from './Views/add-type-car/add-type-car.component';
-import { ListTypeCarComponent } from './Views/list-type-car/list-type-car.component';
+import { AddUserComponent } from './Views/User/add-user/add-user.component';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    HomeComponent,
-    ProfileComponent,
-    TrackingComponent,
     NavbarComponent,
+    LeftbarComponent,
     FooterComponent,
-    ScrollebarComponent,
-    ListeUsersComponent,
-    AddUsersComponent,
-    EditUsersComponent,
-    ListeTrackingComponent,
-    AddCarComponent,
-    ListeCarComponent,
-    EditCarComponent,
-    AddFamilyCarComponent,
-    ListFamilyCarComponent,
-    AddTypeCarComponent,
-    ListTypeCarComponent
+    AddUserComponent,
   ],
   imports: [
     UserModule,
@@ -69,10 +45,8 @@ import { ListTypeCarComponent } from './Views/list-type-car/list-type-car.compon
     CommonModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyCYbZu76P-IhTFCrh3k46SN662xbkgpK68'
-    })
+    AppRoutingModule
+
   ],
   providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
